427089be56ba2394ffa379c0c55be3a65f7ed1b7
Methanusorn <110581337+Methanusorn@users.noreply.github.com>
2024-10-27 17:06:20 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  base: '/portfolio' 
-})
+  base: './',
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
